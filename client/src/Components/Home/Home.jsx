ad686058461706d42537e0de2290c46b75365a85
myaduvanshi <mmukesh1508@gmail.com>
2024-01-15 19:23:53 +0530

    header part 1

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import SlidetopMain from '../Home/SlideBars/SlidetopMain'
+import CardsContainer1 from './CardsContent/CardsContainer1'
+import Slide1 from './SlideBars/Slide1'
+
+function Home() {
+  return (
+    <>
+    <div className='mx-auto w-full max-w-7xl bg-[#E3E6E6]'>
+    {/* <SlidetopMain/> */}
+    <CardsContainer1/>
+    <Slide1/>
+    </div>
+    </>
+  )
+}
+
+export default Home
